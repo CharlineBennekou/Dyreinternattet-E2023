e47f0ef236958c46e3722a43967f2c672aeb14c8
Marjorine Pearl Zalwango <maz001@edu.zealand.dk>
2023-11-27 10:54:10 +0100

    Commentarer

--- Dyreinternattet Semesterprojekt Vinter 2023/Pages/GetAllDyr.cshtml.cs
+++ Dyreinternattet Semesterprojekt Vinter 2023/Pages/GetAllDyr.cshtml.cs
@@ -6,8 +6,11 @@ namespace Dyreinternattet_Semesterprojekt_Vinter_2023.Models
 {
     public class DyreoversigtModel : PageModel
     {
+
+        // Oprettede en liste af Dyr som består af dens construktor
         public List<Dyr> Dyreliste { get; private set; } = new List<Dyr>()
         {
+            // Dyr bliver oprettet af contruktor fra dyr
             new Dyr("Fido", "Husky-mix", 4, 42, true, false),
             new Dyr("Hans", "Huskat", 9, 5.8, true, false)
         };
