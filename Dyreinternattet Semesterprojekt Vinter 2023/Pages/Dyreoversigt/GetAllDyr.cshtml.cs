c2621065be328dd1908a5631cf15c0880152bf63
CharlineBennekou <149783592+CharlineBennekou@users.noreply.github.com>
2023-11-27 12:16:58 +0100

    Link mellem GetAllDyr og service + kommentarer

---
+++ Dyreinternattet Semesterprojekt Vinter 2023/Pages/Dyreoversigt/GetAllDyr.cshtml.cs
@@ -0,0 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Dyreinternattet_Semesterprojekt_Vinter_2023.Models.Dyreoversigt;
+using Dyreinternattet_Semesterprojekt_Vinter_2023.Data;
+using Dyreinternattet_Semesterprojekt_Vinter_2023.Services;
+using Dyreinternattet_Semesterprojekt_Vinter_2023.Pages;
+
+namespace Dyreinternattet_Semesterprojekt_Vinter_2023.Pages.Dyreoversigt
+{
+    public class GetAllDyr : PageModel
+    {
+        private IDyreService _dyreService; //Instance field som kan referere til en service
+        public GetAllDyr(IDyreService dyreService) //Service initialiseres vha. dependency injection
+        {
+            _dyreService = dyreService;
+        }
+        public List<Models.Dyreoversigt.Dyr> Dyreliste { get; private set; } //Property som kan referere til en liste af dyr
+        public void OnGet() //Dyrelisten henter data fra servicen
+        {
+            Dyreliste = _dyreService.GetDyr();
+        }
+    }
+}
