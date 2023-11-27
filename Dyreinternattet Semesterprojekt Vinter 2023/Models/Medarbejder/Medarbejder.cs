0ba83c4fb7aa1080f4b2b4a74a70204ddca55a14
warisz1 <warisah2665@gmail.com>
2023-11-27 10:33:13 +0100

    medarbejder constructor

--- Dyreinternattet Semesterprojekt Vinter 2023/Models/Medarbejder/Medarbejder.cs
+++ Dyreinternattet Semesterprojekt Vinter 2023/Models/Medarbejder/Medarbejder.cs
@@ -2,10 +2,27 @@
 {
     public class Medarbejder
     {
+        static int nextId = 1;
         public string Name { get; set; }
         public int Id { get; set; }
         public string Email { get; set; }
         public int Tlf { get; set; }
 
+
+
+        public Medarbejder()
+        {
+            Id = nextId++;
+        }
+
+        public Medarbejder(string name, string email, int tlf)
+        {
+            Id = nextId++;
+            Name = name;
+            Email = email;
+            Tlf = tlf;
+        }
+
+        
     }
 }
