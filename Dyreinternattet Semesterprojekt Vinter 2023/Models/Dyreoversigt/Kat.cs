010c86831f061021c23da94097bf533629fbbde6
warisz1 <warisah2665@gmail.com>
2023-11-27 11:12:40 +0100

    kat

--- Dyreinternattet Semesterprojekt Vinter 2023/Models/Dyreoversigt/Kat.cs
+++ Dyreinternattet Semesterprojekt Vinter 2023/Models/Dyreoversigt/Kat.cs
@@ -2,6 +2,13 @@
 {
     public class Kat : Dyr
     {
+        public Kat()
+        {
+        }
 
+        public Kat(string navn, string race, double alder, double vægt, bool vaccineStatus, bool erAdopteret, string mad)
+            : base(navn, race, alder, vægt, vaccineStatus, erAdopteret, mad)
+        {
+        }
     }
 }
